26903d0dbce6a71cb63d38573f6d9765a64bf41a
Manuel Astudillo <manuel@optimalbits.com>
2015-07-22 10:31:31 +0200

    added initial players api

--- index.js
+++ index.js
@@ -7,13 +7,15 @@
 /// <reference path="typings/node/node.d.ts"/>
 "use strict";
 
-module.exports = function(networkId, apiToken, opts) {
-	opts = opts || {};
-	var auth = {
-		user: networkId,
-		pass: apiToken		
-	};
-	return {
-		adspaces: require('./lib/adspaces')(auth, opts)	
-	};
+module.exports = function (networkId, apiToken, opts) {
+  opts = opts || {};
+  var auth = {
+    user: networkId,
+    pass: apiToken
+  };
+  var baseUrl = opts.url || 'https://dev.adleria.com/';
+  return {
+    adspaces: require('./lib/adspaces')(auth, baseUrl, opts),
+    players: require('./lib/players')(auth, baseUrl, opts)
+  };
 };
