8de21915bc6f2b867dc4c56e527592645a6804b4
Manuel Astudillo <manuel@optimalbits.com>
2021-02-01 11:07:41 +0100

    feat: replace payoutOrders by invoices

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * Adleria bindings for nodejs
- * 
+ *
  * (c) 2015 Optimal Bits Sweden AB.
- * 
+ *
  */
 /// <reference path="typings/node/node.d.ts"/>
 "use strict";
@@ -11,16 +11,16 @@ module.exports = function (networkId, apiToken, opts) {
   opts = opts || {};
   var auth = {
     user: networkId,
-    pass: apiToken
+    pass: apiToken,
   };
-  var baseUrl = opts.url || 'https://dev.adleria.com/';
+  var baseUrl = opts.url || "https://dev.adleria.com/";
   return {
-    adspaces: require('./lib/adspaces')(auth, baseUrl, opts),
-    players: require('./lib/players')(auth, baseUrl, opts),
-    payoutOrders: require('./lib/payoutOrders')(auth, baseUrl, opts),
-    networks: require('./lib/networks')(auth, baseUrl, opts),
-    tags: require('./lib/tags')(auth, baseUrl, opts),
-    agents: require('./lib/agents')(auth, baseUrl, opts),
-    customers: require('./lib/customers')(auth, baseUrl, opts)
+    adspaces: require("./lib/adspaces")(auth, baseUrl, opts),
+    players: require("./lib/players")(auth, baseUrl, opts),
+    invoices: require("./lib/invoices")(auth, baseUrl, opts),
+    networks: require("./lib/networks")(auth, baseUrl, opts),
+    tags: require("./lib/tags")(auth, baseUrl, opts),
+    agents: require("./lib/agents")(auth, baseUrl, opts),
+    customers: require("./lib/customers")(auth, baseUrl, opts),
   };
 };
